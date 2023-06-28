0fbd55503a846b705a89940f9be95eaf811eb78a
celsj <celsj@mcmaster.ca>
2023-06-28 12:06:17 -0400

    Generalized js expand/hide arrow

--- js/functionality.js
+++ js/functionality.js
@@ -1,15 +1,15 @@
-function testFunction() {
-    var moreArrow1 = document.getElementById("more-arrow-1");
-    var lessArrow1 = document.getElementById("less-arrow-1");
-    var jobDesc1 = document.getElementById("job-desc-1");
+function toggleArrow(idNum) {
+    var moreArrow = document.getElementById("more-arrow-".concat(idNum));
+    var lessArrow = document.getElementById("less-arrow-".concat(idNum));
+    var jobDesc = document.getElementById("job-desc-".concat(idNum));
 
-    if (moreArrow1.style.display === "none") { //Hide
-        moreArrow1.style.display = "block";
-        lessArrow1.style.display = "none";
-        jobDesc1.style.display = "none";
+    if (moreArrow.style.display === "none") { //Hide
+        moreArrow.style.display = "block";
+        lessArrow.style.display = "none";
+        jobDesc.style.display = "none";
     } else { //Show
-        moreArrow1.style.display = "none";
-        lessArrow1.style.display = "block";
-        jobDesc1.style.display = "block";
+        moreArrow.style.display = "none";
+        lessArrow.style.display = "block";
+        jobDesc.style.display = "block";
     }
 }
