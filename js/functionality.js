5df75867f6c6df1feb5317b03d9ae6c7fea28098
celsj <celsj@mcmaster.ca>
2023-07-04 15:24:08 -0400

    Changes to layout

--- js/functionality.js
+++ js/functionality.js
@@ -2,14 +2,20 @@ function toggleArrow(idNum) {
     var moreArrow = document.getElementById("more-arrow-".concat(idNum));
     var lessArrow = document.getElementById("less-arrow-".concat(idNum));
     var jobDesc = document.getElementById("job-expand-wrapper-".concat(idNum));
+    var moreArrowWrapper = document.getElementById("expand-arrow-hide-".concat(idNum));
+    var lessArrowWrapper = document.getElementById("expand-arrow-show-".concat(idNum));
 
     if (moreArrow.style.display === "none") { //Hide
         moreArrow.style.display = "block";
         lessArrow.style.display = "none";
         jobDesc.style.display = "none";
+        moreArrowWrapper.style.display = "inline-block";
+        lessArrowWrapper.style.display = "none";
     } else { //Show
         moreArrow.style.display = "none";
         lessArrow.style.display = "block";
         jobDesc.style.display = "block";
+        moreArrowWrapper.style.display = "none";
+        lessArrowWrapper.style.display = "inline-block";
     }
 }
