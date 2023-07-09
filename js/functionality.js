aab917f2380e82eb98334fec78aaac838110a8bf
celsj <celsj@mcmaster.ca>
2023-07-08 23:13:06 -0400

    Combined nav bar and experience job content

--- js/functionality.js
+++ js/functionality.js
@@ -1,11 +1,10 @@
 function toggleArrow(idNum) {
-    console.log("Touch")
     var moreArrow = document.getElementById("more-arrow-".concat(idNum));
     var lessArrow = document.getElementById("less-arrow-".concat(idNum));
     var jobDesc = document.getElementById("job-expand-wrapper-".concat(idNum));
     var moreArrowWrapper = document.getElementById("expand-arrow-hide-".concat(idNum));
     var lessArrowWrapper = document.getElementById("expand-arrow-show-".concat(idNum));
-
+    var jobWrapper = document.getElementById("job-".concat(idNum));
     if (moreArrow.style.display === "none") { //Hide
         moreArrow.style.display = "inline-block";
         lessArrow.style.display = "none";
@@ -19,4 +18,5 @@ function toggleArrow(idNum) {
         moreArrowWrapper.style.display = "none";
         lessArrowWrapper.style.display = "inline-block";
     }
+    jobWrapper.scrollIntoView({behavior: 'smooth'});
 }
